fe23d0aad7b1fe6832a32aadbcfab3857717055a
David Movsisyan <35527153+davo1999@users.noreply.github.com>
2018-05-02 01:19:58 +0400

    Add files via upload

---
+++ matrix.js
@@ -0,0 +1,51 @@
+const Matrix = (() => {
+    
+    const _isArray = matrix => 
+        Array.isArray(matrix);
+        
+    const _isMatrix = matrix => 
+        _isArray(matrix) && matrix.every(array => _isArray(array));
+        
+    const _isSquare = matrix =>
+        matrix.every(array => array.length === matrix.length);
+    
+    const _isValidMatrix = matrix => {
+        if (!_isMatrix(matrix)) throw new Error('Must be a matrix');
+        if (!_isSquare(matrix)) throw new Error('Must be a square matrix');
+        return true;
+    };
+    
+    const _diagonal = matrix => {
+        let det = 1;
+        for (let i = 0; i < matrix.length; i++) {
+            det *= matrix[i][i];
+        }
+        return (det + 0, det); // -0 is an undesirable result
+    };
+    
+    const _gaussElimination = (row, matrix, index) => {
+        const length = matrix.length;
+        for (let i = index + 1; i < length; i++) {
+            let k = -(matrix[i][index] / row[index]);
+            for (let j = index + 1; j < length; j++) {
+                matrix[i][j] += k * row[j];
+            }
+        }
+        return matrix;
+    };
+      
+    const countDeterminant = matrix => {
+        if (!_isValidMatrix(matrix)) return null;
+        for (let i = 0; i < matrix.length - 1; i++) {
+            matrix = _gaussElimination(matrix[i], matrix, i);
+        }
+        return _diagonal(matrix);
+    };
+    
+    return {
+        det: countDeterminant
+    };
+
+})();
+
+module.exports = Matrix;
