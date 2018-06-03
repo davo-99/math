4246e45450686f7f728e9510a892ab49f4abaafd
David Movsisyan <35527153+davo1999@users.noreply.github.com>
2018-06-04 01:44:04 +0400

    determinant++

--- determinant.cpp
+++ determinant.cpp
@@ -1,66 +1,58 @@
 // #include "stdafx.h"
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 
 using namespace std;
 
-#define MAX 10
+#define MAX 20
 
-void SwapRows(double[][MAX], int, int, int);
-void SortMatrix(double[][MAX], int&, int);
+void PrintMatrix(double[][MAX], int);
+bool IsAllZero(double[][MAX], int);
+int ArgMax(double[][MAX], int, int, int);
 double Diagonal(double[][MAX], int);
-void GaussElimination(double[][MAX], int, int);
+void SwapRows(double[][MAX], int, int, int);
+int GaussElimination(double[][MAX], int);
 double Determinant(double[][MAX], int);
 
 int main() {
 	int n;
-	double matrix[10][10];
-	cout << "Nermucir matrici chapsy\n";
-	do { cin >> n; } while (n < 2 || n > 10);
-	cout << "Nermucir matrici tvery\n";
+	double matrix[20][20];
+	cout << "Input matrix dimension\n";
+	do { cin >> n; } while (n < 2 || n > 20);
+	cout << "Set matrix numbers\n";
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
 			cin >> matrix[i][j];
-	cout << Determinant(matrix, n) << endl;
+	cout << setprecision(5) << fixed << "determinant = " << Determinant(matrix, n) << endl;
 	system("pause");
 	return 0;
 }
 
-void SwapRows(double mat[][MAX], int i, int j, int n) {
-	for (int k = 0; k < n; k++) {
-		double temp = matrix[i][k];
-		matrix[i][k] = matrix[j][k];
-		matrix[j][k] = temp;
+void PrintMatrix(double matrix[][MAX], int n) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)
+			cout << matrix[i][j] << '\t';
+		cout << endl;
 	}
 }
 
-void SortMatrix(double matrix[][MAX], int& swapped, int n) {
-	int nonZeros = 0;
-	int j = 0;
-	for (int i = 0; i < n - 1; i++) {
-		if (matrix[i][j] != 0) {
-			nonZeros++;
-			if (nonZeros == n - 1) {
-				j++;
-				i = -1;
-			}
-			continue;
+bool IsAllZero(double matrix[][MAX], int n) {
+	for (int i = 0; i < n; i++)
+		if (matrix[i][0] != 0) return false;
+	return true;
+}
+
+int ArgMax(double matrix[][MAX], int h, int k, int n) {
+	int max = fabs(matrix[h][k]);
+	int index = h;
+	for (int i = h + 1; i < n; i++) {
+		if (fabs(matrix[i][k]) > max) {
+			max = fabs(matrix[i][k]);
+			index = i;
 		}
-		int index = i + 1;
-		while (index < n) {
-			if (matrix[index][j] != 0) break;
-			index++;
-		}
-		if (index == n) index--;
-		if (matrix[index][j] != 0) {
-			SwapRows(matrix, i, index, n);
-			swapped++;
-			continue;
-		}
-		if (j == n) break;
-		j++;
-		i--;
 	}
+	return index;
 }
 
 double Diagonal(double matrix[][MAX], int n) {
@@ -70,22 +62,41 @@ double Diagonal(double matrix[][MAX], int n) {
 	return det;
 }
 
-void GaussElimination(double matrix[][MAX], int index, int n) {
-	double* row = matrix[index];
-	for (int i = index + 1; i < n; i++) {
-		if (*(row + index) == 0) continue;
-		double k = -(matrix[i][index] / *(row + index));
-		for (int j = index + 1; j < n; j++)
-			matrix[i][j] += k * *(row + j);
-		matrix[i][index] = 0;
+void SwapRows(double mat[][MAX], int i, int j, int n) {
+	for (int k = 0; k < n; k++) {
+		double temp = mat[i][k];
+		mat[i][k] = mat[j][k];
+		mat[j][k] = temp;
 	}
 }
 
-double Determinant(double matrix[][MAX], int n) {
+int GaussElimination(double matrix[][MAX], int n) {
 	int swapCount = 0;
-	SortMatrix(matrix, swapCount, n);
-	for (int i = 0; i < n - 1; i++)
-		GaussElimination(matrix, i, n);
+	for (int h = 0, k = 0; h < n && k < n; k++) {
+		int i_max = ArgMax(matrix, h, k, n);
+		if (matrix[i_max][k] == 0) continue;
+		else {
+			if (i_max != h) {
+				SwapRows(matrix, h, i_max, n);
+				swapCount++;
+			}
+			for (int i = h + 1; i < n; i++) {
+				double f = matrix[i][k] / matrix[h][k];
+				matrix[i][k] = 0;
+				for (int j = k + 1; j < n; j++) {
+					matrix[i][j] -= matrix[h][j] * f;
+				}
+			}
+			h++;
+		}
+	}
+	return swapCount;
+}
+
+double Determinant(double matrix[][MAX], int n) {
+	if (IsAllZero(matrix, n)) return 0;
+	int swapCount = GaussElimination(matrix, n);
 	double result = Diagonal(matrix, n) * pow(-1, swapCount) + 0;
+	PrintMatrix(matrix, n);
 	return result;
 }
