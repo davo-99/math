54b6763fec78cbc110f3871854d9050446689131
David Movsisyan <35527153+davo1999@users.noreply.github.com>
2018-06-04 00:22:01 +0400

    determinant
    Counts the determinant.

--- determinant.js
+++ determinant.js
@@ -17,7 +17,8 @@ const Matrix = (() => {
     
     const _fix = number => +number.toFixed(10);
     
-    const _isAllZero = matrix => matrix.every(row => !row[0]);
+    const _isAllZero = matrix =>
+        matrix.every(row => !row[0]);
     
     const _argmax = (matrix, h,  k) => {
         const length = matrix.length;
@@ -35,7 +36,7 @@ const Matrix = (() => {
         let det = 1;
         for (let i = 0; i < matrix.length; i++)
             det *= matrix[i][i];
-        return (det += 0, det); // -0 is an undesirable result
+        return det;
     };
     
     const _swapRows = (matrix, i, j) =>
@@ -48,14 +49,15 @@ const Matrix = (() => {
             if (matrix[i_max][k] === 0) continue;
             else {
                 if (i_max !== h) {
-                    matrix = _swapRows(matrix, h, i_max);
+                    _swapRows(matrix, h, i_max);
                     counter.swapped++;
                 }
                 for (let i = h + 1; i < length; i++) {
                     let f = matrix[i][k] / matrix[h][k];
                     matrix[i][k] = 0;
-                    for (let j = k + 1; j < length; j++)
+                    for (let j = k + 1; j < length; j++) {
                         matrix[i][j] -= matrix[h][j] * f;
+                    }
                 }
                 h++;
             }
@@ -68,7 +70,7 @@ const Matrix = (() => {
         if (_isAllZero(matrix)) return 0;
         const counter = { swapped: 0 };
         matrix = _gaussElimination(matrix, counter);
-        let result = _diagonal(matrix) * ((-1) ** counter.swapped) + 0;
+        let result = _diagonal(matrix) * (-1) ** counter.swapped + 0;
         return _fix(result);
     };
     
