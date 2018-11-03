95cb0d39c968eb2e3d8676eba90d9f6729590b8d
David Movsisyan <35527153+davo1999@users.noreply.github.com>
2018-11-03 18:03:03 +0400

    shortened :p

--- determinant.js
+++ determinant.js
@@ -15,32 +15,25 @@ const Matrix = (() => {
         return true;
     };
     
-    const _fix = number => +number.toFixed(10);
-    
     const _isAllZero = matrix =>
         matrix.every(row => !row[0]);
     
-    const _argmax = (matrix, h,  k) => {
-        const length = matrix.length;
-        let max = Math.abs(matrix[h][k]);
-        let index = h;
-        for (let i = h + 1; i < length; i++)
-            if (Math.abs(matrix[i][k]) > max) {
-                max = Math.abs(matrix[i][k]);
-                index = i;
-            }
-        return index;
-    };
+    const _fix = number => +number.toFixed(10);
     
-    const _diagonal = matrix => {
-        let det = 1;
-        for (let i = 0; i < matrix.length; i++)
-            det *= matrix[i][i];
-        return det;
-    };
-    
+    const _diagonal = matrix => 
+        matrix.reduce((a, r, i) => a * r[i], 1);
+
     const _swapRows = (matrix, i, j) =>
         [matrix[i], matrix[j]] = [matrix[j], matrix[i]];
+
+    const _argmax = (matrix, h,  k) => {
+        const length = matrix.length;
+        let max = Math.abs(matrix[h][k]), index = h;
+        for (let i = h + 1; i < length; i++)
+            if (Math.abs(matrix[i][k]) > max)
+                (max = Math.abs(matrix[i][k]), index = i);
+        return index;
+    };
     
     const _gaussElimination = (matrix, counter) => {
         const length = matrix.length;
@@ -48,10 +41,8 @@ const Matrix = (() => {
             let i_max = _argmax(matrix, h, k);
             if (matrix[i_max][k] === 0) continue;
             else {
-                if (i_max !== h) {
-                    _swapRows(matrix, h, i_max);
-                    counter.swapped++;
-                }
+                if (i_max !== h)
+                    (_swapRows(matrix, h, i_max), counter.swapped++);
                 for (let i = h + 1; i < length; i++) {
                     let f = matrix[i][k] / matrix[h][k];
                     matrix[i][k] = 0;
@@ -69,7 +60,7 @@ const Matrix = (() => {
         if (_isAllZero(matrix)) return 0;
         const counter = { swapped: 0 };
         matrix = _gaussElimination(matrix, counter);
-        let result = _diagonal(matrix) * (-1) ** counter.swapped + 0;
+        const result = _diagonal(matrix) * (-1) ** counter.swapped + 0;
         return _fix(result);
     };
     
